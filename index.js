5478d4c2e7aaeb58091e52807928cbcd461d66b7
isroiljonmusajonov <isroiljonmusaj@gmail.com>
2022-01-09 15:11:38 +0500

    ancha narsaini torladim

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const menuBtn = document.querySelector(".menu-btn");
 const nav = document.getElementById("nav");
+const toTop = document.querySelector(".to-top");
+const box1 = document.getElementById("box1");
+const box2 = document.getElementById("box2");
+const box3 = document.getElementById("box3");
+const cards2 = document.getElementById("Вопрос2");
+const cards1 = document.getElementById("Вопрос1");
+const cards3 = document.getElementById("Вопрос3");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
@@ -12,4 +27,13 @@ menuBtn.addEventListener("click", () => {
 });
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("active");
+});
+box2.addEventListener("click", () => {
+  cards2.classList.toggle("active");
+});
+box1.addEventListener("click", () => {
+  cards1.classList.toggle("active");
+});
+box3.addEventListener("click", () => {
+  cards3.classList.toggle("active");
 });
